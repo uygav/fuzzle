d0cda54d6ccc58155bfbfa63edb9c82115d4427c
Uygar Muntaş <muntasuygar@gmail.com>
2023-08-14 02:15:51 +0300

    signin and logout

--- core/urls.py
+++ core/urls.py
@@ -3,5 +3,7 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
-    path('signup', views.signup, name='signup')  # url something
+    path('signup', views.signup, name='signup'),  # url something
+    path('signin', views.signin, name='signin'),  # url for signin page
+    path('logout', views.logout, name='logout')
 ]
--- core/views.py
+++ core/views.py
@@ -3,11 +3,14 @@ from django.shortcuts import render, redirect  # i added redirect
 from django.contrib.auth.models import User, auth
 from django.contrib import messages  # import for error message
 from django.http import HttpResponse
+# this import checks of user entered the page
+from django.contrib.auth.decorators import login_required
 from .models import Profile  # i imported Profile class
 
 # Create your views here.
 
 
+@login_required(login_url='signin')  # user cannot access directly to main menu
 def index(request):
     return render(request, 'index.html')
 
@@ -46,3 +49,26 @@ def signup(request):  # request parameter means call http file
 
     else:
         return render(request, 'signup.html')
+
+
+def signin(request):
+    if request.method == 'POST':
+        username = request.POST['username']
+        password = request.POST['password']
+
+        user = auth.authenticate(username=username, password=password)
+
+        if user is not None:
+            auth.login(request, user)
+            return redirect('/')
+        else:
+            messages.info(request, 'Credentials Invalid')
+            return redirect('signin')
+
+    else:
+        return render(request, 'signin.html')
+
+
+def logout(request):
+    auth.logout(request)
+    return redirect('signin')
